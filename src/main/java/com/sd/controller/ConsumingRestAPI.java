55d8e5b788c7f28eaecbb3c68500e52fe5f7de1e
sreesailam <sreesailam.dasari@gmail.com>
2018-06-11 17:56:26 +0530

    removed un imports

--- src/main/java/com/sd/controller/ConsumingRestAPI.java
+++ src/main/java/com/sd/controller/ConsumingRestAPI.java
@@ -11,15 +11,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import com.sd.model.Employee;
 import com.sd.model.Request;
 
